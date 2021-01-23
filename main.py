7323c597ace6b069dd96c401646d2d1d82667662
josephflu <joseph@fluckiger.org>
2021-01-23 15:29:42 -0600

    update reverse method

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 
-import discord
 from discord.ext import commands
-
 
 # all commands will be of the form "!command-name" (or whatever prefix you choose)
 bot = commands.Bot(command_prefix='!')
@@ -18,31 +16,31 @@ async def my_cool_command(context):
     # ...and in the chat
     await context.send(guild)
 
-# name is the name you type on the server when you want to invoke the commmand
+
 @bot.command()
-async def reverse(context,*args):
-    # if you recall, a guild is just a fancy name for a server
-    reversed = args[::-1]
-    #guild = str(context.guild)
-    # now we print it in the terminal...
-    print(reversed )
-    # ...and in the chat
-    await context.send(reversed)
+async def reverse(context):
+    print("processing !reverse")
+    msg = context.message.content[9:]  # remove first 9 characters '!reverse '
+    rev = msg[::-1]
+    await context.send(rev)
 
-@bot.event
-async def on_message(message):
-    if bot.user!=message.author:
-        if 'cat' in message.content:
-            print('cat')
-            await message.channel.send('cat')
 
-def get_discord_bot_token():
-    # tokens are usually passed to the script as environment variables
-    # but you can also store them in a file (you will need to change this function)
-    return os.getenv('DISCORD_TOKEN')
+# @bot.event
+# async def on_message(message):
+#     if bot.user!=message.author:
+#         if 'cat' in message.content:
+#             print('cat')
+#             await message.channel.send('cat')
+
+# @bot.event
+# async def on_message(message):
+#     if bot.user != message.author:
+#         msg = f"well hi there {message.author.name}, thanks for saying '{message.content}'"
+#         await message.channel.send(msg)
 
 
 if __name__ == "__main__":
-    token = get_discord_bot_token()
+    token = os.getenv('DISCORD_TOKEN')
+    print("running MYDBOT")
     bot.run(token)
 

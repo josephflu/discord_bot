8f20b8fc79d3a4df27a2a5366fd3a17e7e070f3e
lego mike <joseph@fluckiger.org>
2021-01-23 15:05:40 -0600

    add code comment

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ async def my_cool_command(context):
     # ...and in the chat
     await context.send(guild)
 
+# name is the name you type on the server when you want to invoke the commmand
+@bot.command()
+async def reverse(context,*args):
+    # if you recall, a guild is just a fancy name for a server
+    reversed = args[::-1]
+    #guild = str(context.guild)
+    # now we print it in the terminal...
+    print(reversed )
+    # ...and in the chat
+    await context.send(reversed)
+
+@bot.event
+async def on_message(message):
+    if bot.user!=message.author:
+        if 'cat' in message.content:
+            print('cat')
+            await message.channel.send('cat')
 
 def get_discord_bot_token():
     # tokens are usually passed to the script as environment variables

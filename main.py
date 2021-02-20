8113a849b827b823aa281f281a55a5c919609977
lego mike <joseph@fluckiger.org>
2021-02-20 14:51:02 -0600

    add code comment

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
+
 from cog import Task
-from discord.ext import commands
+from discord.ext import commands, tasks
 from pyjokes import get_joke
 
 # all commands will be of the form "!command-name" (or whatever prefix you choose)
 bot = commands.Bot(command_prefix='!')
-bot.add_cog(Task(bot))
+#bot.add_cog(Task(bot))
 
 # name is the name you type on the server when you want to invoke the commmand
 @bot.command(name='my-cool-command')
@@ -32,6 +33,24 @@ async def reverse(context):
     rev = msg[::-1]
     await context.send(rev)
 
+@bot.command()
+async def move(context):
+    print("moving channel member")
+    #move person over in channel
+    all_channels = context.guild.voice_channels
+    afkChannel = None
+    for channel in all_channels:
+        if 'AFK' in channel.name:
+            afkChannel = channel
+            break
+    if afkChannel is None:
+        print("AFK channel not found")
+        return
+    print("AFK channel found")
+    user = context.message.author
+    await user.move_to(afkChannel)
+    #move user to AFK channel.
+
 
 @bot.event
 async def on_message(message):
@@ -41,6 +60,19 @@ async def on_message(message):
             await message.channel.send('cat')
 
     await bot.process_commands(message)
+
+@tasks.loop(seconds=3)
+async def repeat():
+    print("h  e  l  o")
+
+    guild = bot.guilds[0]
+    members = guild.members
+    for vc in guild.voice_channels:
+        if vc.name.startswith("AFK"):
+            afk_channel = vc
+            break
+    for member in members:
+        await member.move_to(afk_channel)
 
 # @bot.event
 # async def on_message(message):
@@ -53,5 +85,6 @@ if __name__ == "__main__":
     token = os.getenv('DISCORD_TOKEN')
     print(token)
     print("running MYDBOT")
+    repeat.start()
     bot.run(token)
 
